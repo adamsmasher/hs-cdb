265debb818f3b5f24fd070a24876aeb12f8d1810
Adam Smith <adam@tablethotels.com>
2012-01-15 00:48:04 -0500

    a first, very rough attempt to add write support
    Signed-off-by: Adam Smith <adam@tablethotels.com>

--- Database/CDB.hs
+++ Database/CDB.hs
@@ -4,7 +4,7 @@
 --
 -- License     : BSD-style
 --
--- A library for reading CDB (Constant Database) files.
+-- A library for reading and writing CDB (Constant Database) files.
 --
 -- CDB files are immutable key-value stores, designed for extremely fast and
 -- memory-efficient construction and lookup. They can be as large as 4GB, and
@@ -33,8 +33,20 @@
 -- automatically pack and unpack keys and values to suit the types you're using
 -- in your program. In particular, in an instance is provided for 'String', so
 -- @hs-cdb@ can use 'String's as keys and values transparently.
+--
+-- Writing a CDB is just as straightforward:
+--
+-- >    makeCDB :: IO ()
+-- >    makeCDB = cdbMake "my.cdb" $ do
+-- >      cdbAdd "foo" "this is the data associated with foo"
+-- >      cdbAddMany [("bar1", "bar1data"), ("bar2", "bar2data")]
+--
+-- Again, @hs-cdb@ automatically closes the files after use. Moreover, in CDB
+-- tradition, @hs-cdb@ actually creates a CDB named @file.cdb@ by first writing 
+-- it to @file.cdb.tmp@, and then atomically renaming it over @file.cdb@. This
+-- means that readers never need to pause when you're regenerating a CDB.
 
-{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE FlexibleInstances, TypeSynonymInstances #-}
 
 module Database.CDB (
   -- * The @CDB@ type
@@ -42,14 +54,22 @@ module Database.CDB (
   -- * Classes
   Packable,
   Unpackable,
-  -- * Interface
+  -- * Reading interface
   cdbInit,
   cdbGet,
   cdbGetAll,
   cdbHasKey,
-  cdbCount) where
+  cdbCount,
+  -- * Writing interface
+  cdbMake,
+  cdbAdd,
+  cdbAddMany) where
 
+import Control.Applicative
 import Control.Monad
+import Control.Monad.State
+import Data.Array.IO
+import Data.Array.Unboxed
 import Data.Bits
 import qualified Data.ByteString as ByteString
 import qualified Data.ByteString.Char8 as ByteString.Char8
@@ -57,9 +77,11 @@ import Data.ByteString (ByteString)
 import Data.Char
 import Data.List
 import Data.Word
+import Directory
+import System.FilePath
+import System.IO
 import System.IO.Posix.MMap
 
--- $usage
 ------------
 -- interface
 ------------
@@ -93,6 +115,16 @@ instance Packable [Word8] where
 instance Unpackable [Word8] where
   unpack = ByteString.unpack
 
+instance Packable Word32 where
+  pack n = ByteString.pack $ map fromIntegral 
+    [n .&. 0xFF,
+    (n `shiftR` 8) .&. 0xFF,
+    (n `shiftR` 16) .&. 0xFF,
+    (n `shiftR` 24) .&. 0xFF]
+
+instance Packable (Array Word32 Word32) where
+  pack t = pack $ ByteString.concat $ map pack $ elems t
+
 -- |Loads a CDB from a file.
 cdbInit :: FilePath -> IO CDB
 cdbInit f = liftM CDB $ unsafeMMapFile f 
@@ -117,6 +149,115 @@ cdbHasKey cdb key = case cdbFind cdb (pack key) of
 cdbCount :: (Packable k) => CDB -> k -> Int
 cdbCount cdb key = length $ cdbFind cdb (pack key)
 
+cdbMake :: FilePath -> CDBMake -> IO ()
+cdbMake fileName f = do
+  let tmp = fileName <.> "tmp"
+  h <- openBinaryFile tmp WriteMode
+  hSeek h AbsoluteSeek (256*8)
+  initState <- initialMakeState h
+  cdb <- execStateT f initState
+  writeHashTables cdb
+  hClose h
+  renameFile tmp fileName
+
+-- |Adds a given key-value pair to a CDB
+cdbAdd :: (Packable k, Packable v) => k -> v -> CDBMake
+cdbAdd k v = do
+  let (pk, pv) = (pack k, pack v)
+  cdbAddSlot pk pv
+  cdbWriteRecord pk pv
+
+cdbAddMany :: (Packable k, Packable v) => [(k,v)] -> CDBMake
+cdbAddMany = mapM_ (uncurry cdbAdd)
+
+-----------------------
+-- write implementation
+-----------------------
+
+type CDBMake = StateT CDBMakeState IO ()
+
+data CDBMakeState = CDBMakeState {
+  cdbstBuffer        :: IOUArray Word32 Word32,
+  cdbstTablePointers :: IOUArray Word8 Word32,
+  cdbstTableSizes    :: IOUArray Word8 Word32,
+  cdbstHandle        :: Handle,
+  cdbstRecordsEnd    :: Word32
+}
+
+initialMakeState :: Handle -> IO CDBMakeState
+initialMakeState h = do
+  buf <- newArray (0, 256*512) 0
+  tableSizes <- newArray (0, 255) 256
+  pointers <- newListArray (0, 255) [0, (256*2)..]
+  return $ CDBMakeState {
+    cdbstBuffer        = buf,
+    cdbstTableSizes    = tableSizes,
+    cdbstTablePointers = pointers,
+    cdbstRecordsEnd    = 256*8,
+    cdbstHandle        = h
+  }
+
+-- add a slot to the set of slots
+cdbAddSlot :: ByteString -> ByteString -> CDBMake
+cdbAddSlot k v = do
+  let hash     = cdbHash k
+  let tableNum = fromIntegral $ hash `mod` 256
+  cdb <- get
+  let buf = cdbstBuffer cdb
+  tableIndex <- liftIO $ readArray (cdbstTablePointers cdb) tableNum
+  tl <- liftIO $ readArray (cdbstTableSizes cdb) tableNum
+  slot <- liftIO $ cdbFindEmptySlot buf (tableIndex +
+                                            (hash `div` 256 `mod` tl) * 2)
+  pointer <- liftIO $ fromIntegral <$> hTell (cdbstHandle cdb)
+  liftIO $ writeArray buf slot hash
+  liftIO $ writeArray buf (slot+1) pointer
+
+cdbFindEmptySlot :: IOUArray Word32 Word32 -> Word32 -> IO Word32
+cdbFindEmptySlot buf i = do
+  pointer <- readArray buf (i+1)
+  case pointer of
+    0 -> return i
+    _ -> cdbFindEmptySlot buf (i+2)
+
+cdbWriteRecord :: ByteString -> ByteString -> CDBMake
+cdbWriteRecord k v = 
+  let lk  = fromIntegral $ ByteString.length k
+      lv  = fromIntegral $ ByteString.length v
+      record = ByteString.concat [pack lk, pack lv, k, v]
+  in do
+    cdb <- get
+    liftIO $ ByteString.hPut (cdbstHandle cdb) record
+    put $ cdb { cdbstRecordsEnd = (cdbstRecordsEnd cdb) + lk + lv + 8 }
+
+-- assumes the Handle is pointing to right after the last record written
+writeHashTables :: CDBMakeState -> IO ()
+writeHashTables cdb = do
+  writeHashTables' cdb
+  writePointers cdb
+
+-- writes the Hash Tables specficially, not the pointers
+writeHashTables' :: CDBMakeState -> IO ()
+writeHashTables' cdb = do
+  buf <- freeze $ cdbstBuffer cdb
+  ByteString.hPut (cdbstHandle cdb) (pack (buf :: Array Word32 Word32))
+
+writePointers :: CDBMakeState -> IO ()
+writePointers cdb = do
+  hSeek (cdbstHandle cdb) AbsoluteSeek 0
+  pointers <- map ((+(cdbstRecordsEnd cdb)) . (*4))  <$>
+              elems <$>
+              (freeze (cdbstTablePointers cdb) :: IO (Array Word8 Word32))
+  sizes    <- elems <$>
+              (freeze (cdbstTableSizes cdb) :: IO (Array Word8 Word32))
+  mapM_ (writePointer $ cdbstHandle cdb) (zip pointers sizes)
+
+-- writes to the given Handle a pointer to the given hashtable
+-- assumes the handle is pointing to the right place
+writePointer :: Handle -> (Word32, Word32) -> IO ()
+writePointer h (pos, len) = do
+  ByteString.hPut h (pack pos) -- write pointer
+  ByteString.hPut h $ pack ((fromIntegral len) :: Word32)  -- write length
+
 -----------------
 -- implementation
 -----------------
@@ -136,7 +277,6 @@ tableLength cdb n = cdb `cdbRead32` ((fromIntegral n * 8) + 4)
 
 tableOffset :: CDB -> Word8 -> Word32
 tableOffset cdb n = cdb `cdbRead32` (fromIntegral n * 8)
-
 
 -- gets the hash value for a key
 cdbHash :: ByteString -> Word32
