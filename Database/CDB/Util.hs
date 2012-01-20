fb35501f81ed11f3c322ea6dc285436104b81a49
Adam Smith <adam@tablethotels.com>
2012-01-20 11:37:10 -0500

    use ByteString's fold! much better style
    Signed-off-by: Adam Smith <adam@tablethotels.com>

--- Database/CDB/Util.hs
+++ Database/CDB/Util.hs
@@ -3,11 +3,9 @@ module Database.CDB.Util (cdbHash) where
 import qualified Data.ByteString as ByteString
 import Data.ByteString (ByteString)
 import Data.Bits
-import Data.List
 import Data.Word
 
 -- gets the hash value for a key
 cdbHash :: ByteString -> Word32
 cdbHash =
-  foldl' (\h c -> ((h `shiftL` 5) + h) `xor` fromIntegral c) 5381 .
-  ByteString.unpack
+  ByteString.foldl' (\h c -> ((h `shiftL` 5) + h) `xor` fromIntegral c) 5381
